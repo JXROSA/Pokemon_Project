dd131690b4bccbb0c70c81a61b2763095f422762
James Rosa <JROSA10.tv@gmail.com>
2023-01-29 14:41:04 -0500

    first commit

---
+++ XpCalculator/xpCalculator.js
@@ -0,0 +1,51 @@
+// const lvlFifty = 176000000;
+// const userXp = "";
+// const eggCost = "";
+// const raidCost = "";
+// const coin_cost = ["100 coins", "550 coins", "1,200 coins", "2,500 coins", "5,200 coins", "14,500 coins"]
+
+const submit = document.getElementById("submit");
+submit.addEventListener("click", user_XP_input);
+
+const check = document.getElementById("Check");
+check.addEventListener("click", lvl_check);
+
+function user_XP_input() {
+ return (
+  console.log(userXP.value)
+ )
+}
+
+function lvl_check() {
+ var xpvalues = [0, 10, 200, 3000, 40000];
+ const xp_lvl = xpvalues[2]
+ console.log(xp_lvl)
+}
+
+
+0, 0  1, 5000 
+
+input user xp = 10, 000, 000 >= i > 400000 >= i++
+
+function checkXP() {
+ if (userXP.value === xp_lvl) {
+  return console.log("you are lvl")
+ }
+}
+
+
+
+
+
+// alert(userXp = input("Please enter your xp"))
+
+
+
+// 1400 = 1950 raids
+// 3 raids per 1 egg = 240, 000 xp
+// 800 eggs
+
+// 156, 000, 000xp
+// 20, 000, 000
+
+
