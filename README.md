d283d30090f2c6cdd0af4e433acf305ba5bebbe5
James Rosa <JROSA10.tv@gmail.com>
2023-02-15 21:55:23 -0500

    pogocalc statsdisplay

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Pokemon_Project
+
+<!-- const submit = document.getElementById("submit");
+submit.addEventListener("click", user_XP_input);
+
+const check = document.getElementById("checkLvl");
+check.addEventListener("click", lvl_check);
+
+function user_coin_cost() {
+ return (
+  console.log(coin_cost)
+ )
+}
+
+function user_XP_input() {
+ return (
+  console.log(userXP.value)
+ )
+}
+
+function lvl_check() {
+ var xpvalues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
+ const xp_lvl = xpvalues[39]
+ console.log(xp_lvl)
+}
+
+function checkXP() {
+ if (userXP.value === xp_lvl) {
+  return console.log("you are lvl")
+ }
+}
+
+function xp_remaining() {
+ return (176000000 - checkXP())
+}
+
+var xpvalues = { 0: "0", 1: "", 2: "", 3: "", 4: "", 5: "", 6: "", 7: "", 8: "", 9: "", 10: "", 11: "", 12: "", 13: "", 14: "", 15: "", 16: "", 17: "", 18: "", 19: "", 20: "", 21: "", 22: "", 23: "", 24: "", 25: "", 26: "", 27: "", 28: "", 29: "", 30: "", 31: "", 32: "", 33: "", 34: "", 35: "", 36: "", 37: "", 38: "", 39: "", 40: "", 41: "", 42: "", 43: "", 44: "", 45: "", 46: "", 47: "", 48: "", 49: "", 50: "" }; -->
--- XpCalculator/xpCalculator.js
+++ XpCalculator/xpCalculator.js
@@ -1,45 +1,113 @@
-// const lvlFifty = 176000000;
-// const userXp = "";
-// const eggCost = "";
-// const raidCost = "";
-// const coin_cost = ["100 coins", "550 coins", "1,200 coins", "2,500 coins", "5,200 coins", "14,500 coins"]
+// const lvlFifty = 156,000,000 XP;
+// GET / api / v1 / player_xp_requirements.json
+// let userXp = window.prompt('How much XP do you currently have');
+// let lvlFifty = 156000000 - userXp;
+// document.write('You have ' + userXp + ' XP. You need ' + lvlFifty + ' xp reach level fifty ')
 
-const submit = document.getElementById("submit");
-submit.addEventListener("click", user_XP_input);
+let levelCheck;
 
-const check = document.getElementById("Check");
-check.addEventListener("click", lvl_check);
+document.getElementById('submit').onclick = function () {
 
-function user_XP_input() {
- return (
-  console.log(userXP.value)
- )
-}
-
-function lvl_check() {
- var xpvalues = [0, 10, 200, 3000, 40000];
- const xp_lvl = xpvalues[2]
- console.log(xp_lvl)
-}
-
-
-0, 0  1, 5000 
-
-input user xp = 10, 000, 000 >= i > 400000 >= i++
-
-function checkXP() {
- if (userXP.value === xp_lvl) {
-  return console.log("you are lvl")
+ //to round things up/down
+ function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
  }
+ levelCheck = document.getElementById('userXpInput').value;
+ console.log(levelCheck);
+ // Xp needed to reach level 50
+ lvlNeeded = round(176000000 - levelCheck)
+ console.log('you need ' + lvlNeeded + ' XP to reach level 50');
+ // complete this many raids to reach level 50 (no luckyegg no friends)
+ lvlRaid = round(lvlNeeded / 10000);
+ console.log(lvlRaid + ': 5 star raids need to be done with no lucky egg or friends');
+ // complete this many raids with lucky egg active but no friends to reach level 50
+ lvlRaidEgg = round(lvlNeeded / 20000);
+ console.log(lvlRaidEgg + ': 5 star raids need to be done with lucky egg and no friends');
+ // complete this many raids with lucky egg active and 10 new friends to reach level 50
+ lvlRaidEggFriends = round(lvlNeeded / 80000, 2);
+ console.log(lvlRaidEggFriends + ': 5 star raids need to be done with lucky egg and 10 new friends');
+ // how many hours/coins it would take being the most efficient possible by yourself
+ hoursRaiding = round(lvlNeeded / 2160000);
+ coinsSpentPHour = round(2825 * hoursRaiding);
+ console.log('it will take you ' + hoursRaiding + ' hours' + ' or ' + (hoursRaiding / 24) + ' days ' + 'to get to level 50. A total of ' + coinsSpentPHour + ' coins');
+ // how many dollars it took
+ oneHD = round(coinsSpentPHour / 14500)
+ console.log('it took $' + oneHD * 100);
+
+ // personal stats display on page
+ document.getElementById('pRaid').innerHTML = 'you have done a total of ' + (17600 - lvlRaid) + ' raids with no egg or friends... or'
+ document.getElementById('pRaidEgg').innerHTML = 'you have done a total of ' + (8800 - lvlRaidEgg) + ' raids with a lucky egg... or'
+ document.getElementById('pRaidEggFriends').innerHTML = 'you have done a total of ' + (2200 - lvlRaidEggFriends) + ' raids with a lucky egg and 10 new friends or some mix of all three of these'
+ document.getElementById('pHoursRaiding').innerHTML = 'you have done a total of ' + (81 - hoursRaiding) + ' hours raiding'
+ document.getElementById('pOneHD').innerHTML = 'you have/wouldve spent a total of $' + (1600 - (oneHD * 100))
+
+ // to reach level 50 display on page
+ document.getElementById('dLvlNeeded').innerHTML = 'you need ' + lvlNeeded + ' XP to reach level 50'
+ document.getElementById('dLvlRaid').innerHTML = lvlRaid + ': 5 star raids need to be done with no lucky egg or friends'
+ document.getElementById('dLvlRaidEgg').innerHTML = lvlRaidEgg + ': 5 star raids need to be done with lucky egg and no friends'
+ document.getElementById('dLvlRaidEggFriends').innerHTML = lvlRaidEggFriends + ': 5 star raids need to be done with lucky egg and 10 new friends'
+ document.getElementById('dhoursRaiding').innerHTML = 'it will take you ' + hoursRaiding + ' hours' + ' or ' + (hoursRaiding / 24) + ' days ' + 'to get to level 50. A total of ' + coinsSpentPHour + ' coins'
+ document.getElementById('doneHD').innerHTML = 'it will cost you $' + oneHD * 100
 }
 
 
+// you need 176000000 XP to reach level 50
+
+// 17600: 5 star raids need to be done with no lucky egg or friends
+
+// 8800: 5 star raids need to be done with lucky egg and no friends
+
+// 2200: 5 star raids need to be done with lucky egg and 10 new friends
+
+// it will take you 81 hours or 3.375 days to get to level 50. A total of 228825 coins
+
+// it will cost you $1600
 
 
+
+
+
+
+
+
+
+
+
+// lvlraidcost =
+
+// how to check how many times 8 fits into a number
+// rules :
+//  max amount of raids per hour is 27
+// 13.5 per hour 2 lucky eggs
+// 500/8= 62.5 coins 
+// 125 coins per hour 
+// plus 2700 coins for the raids
+// 2825 coins per hour spent doing 27 raids which equals a total xp of 80000*27= 2,160,000
+
+
+
+
+
+
+
+
+
+const eggCost = {
+ 1: "80 coins",
+ 8: "500 coins"
+};
+const coin_cost = {
+ 1: "100 coins",
+ 5: "550 coins",
+ 10: "1,200 coins",
+ 20: "2,500 coins",
+ 40: "5,200 coins",
+ 100: "14,500 coins"
+};
+const raidCost = coin_cost[1];
 
 // alert(userXp = input("Please enter your xp"))
-
-
 
 // 1400 = 1950 raids
 // 3 raids per 1 egg = 240, 000 xp
